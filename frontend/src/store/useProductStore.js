c1a967b213977ae21a47bd2bb8d433e1cfcd4fd4
hazemabuzaid <hazemabuzaid16@gmail.com>
2026-08-30 23:49:36 +0300

    fix api updateproduct

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -96,7 +96,7 @@ export const useProductStore = create((set, get) => ({
         set({ loading: true })
         try {
             const { formData } = get()
-            const response = await axios.post(`${BASE_URL}/api/products/${id}`, formData)
+            const response = await axios.put(`${BASE_URL}/api/products/${id}`, formData)
             set({ currentProduct: response.data.data })
             toast.success("Product updated successfully")
         } catch (err) {
