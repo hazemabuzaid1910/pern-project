aa51f45034e14aa414b4cd1d9998368761445de0
hazemabuzaid <hazemabuzaid16@gmail.com>
2026-09-01 00:03:43 +0300

    add protecte middleware

--- api/routers/ProductRouters.js
+++ api/routers/ProductRouters.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { getAllProducts, createProduct, updateProduct, deleteProduct, getProductById } from "../controllers/ProductController.js";
+
+import {
+    getAllProducts,
+    createProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
+} from "../controllers/ProductController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
+router.get("/products", getAllProducts);
 
-router.get('/products', getAllProducts);
-router.get('/products/:id', getProductById);
-router.post('/products', createProduct);
-router.put('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get("/products/:id", getProductById);
+
+router.post("/products", protect, createProduct);
+
+router.put("/products/:id", protect, updateProduct);
+
+router.delete("/products/:id", protect, deleteProduct);
 
 export default router;
